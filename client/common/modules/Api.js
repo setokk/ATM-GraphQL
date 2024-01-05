639ad8166816d48f176dc10a644dbe0972c770ea
setokk <tesmtracks@gmail.com>
2024-01-05 18:48:41 +0200

    Finished client

--- client/common/modules/Api.js
+++ client/common/modules/Api.js
@@ -1,7 +1,3 @@
 export class Api {
-    static USERS    = 'http://localhost:8080/api/users';
-    static REGISTER = this.USERS + '/register';
-    static LOGIN    = this.USERS + '/login';
-    static DEPOSIT  = this.USERS + '/deposit';
-    static WITHDRAW = this.USERS + '/withdraw';
+    static GRAPHQL = 'http://localhost:8080/graphql';
 }
